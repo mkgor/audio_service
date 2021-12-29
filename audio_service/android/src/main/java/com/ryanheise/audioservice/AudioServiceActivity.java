d648e4dc026d603cbbfd9e09bad93b9ac9e4d5dc
Gor Mkhitaryan <gor.mkhitaryan@rt.ru>
2021-12-29 14:26:28 +0300

    Added fragment activity for audio_service

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -1,14 +1,20 @@
 package com.ryanheise.audioservice;
 
 import android.content.Context;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.FlutterEngineCache;
-import io.flutter.embedding.engine.dart.DartExecutor;
 
-public class AudioServiceActivity extends FlutterActivity {
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class AudioServiceFragmentActivity extends FlutterFragmentActivity {
     @Override
-    public FlutterEngine provideFlutterEngine(Context context) {
+    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
+    }
+
+    protected String getCachedEngineId() {
+        AudioServicePlugin.getFlutterEngine(this);
+        return AudioServicePlugin.getFlutterEngineId();
     }
 }
