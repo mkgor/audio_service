f6f334970ae165384834ae450f6e9b86340bd541
Gor Mkhitaryan <gor.mkhitaryan@rt.ru>
2022-01-11 11:19:01 +0300

    Fixed bug with class naming

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -1,20 +1,14 @@
 package com.ryanheise.audioservice;
 
 import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
-public class AudioServiceFragmentActivity extends FlutterFragmentActivity {
+public class AudioServiceActivity extends FlutterActivity {
     @Override
-    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
+    public FlutterEngine provideFlutterEngine(Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
-    }
-
-    protected String getCachedEngineId() {
-        AudioServicePlugin.getFlutterEngine(this);
-        return AudioServicePlugin.getFlutterEngineId();
     }
 }
