16ed01643254c9f8b08da6dd4daf9c2d11034e02
Gor Mkhitaryan <gor.mkhitaryan@rt.ru>
2022-01-20 13:02:45 +0300

    Trying to fix bug with crashes

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceFragmentActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceFragmentActivity.java
@@ -8,6 +8,7 @@ import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class AudioServiceFragmentActivity extends FlutterFragmentActivity {
+
     @Override
     public FlutterEngine provideFlutterEngine(@NonNull Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
